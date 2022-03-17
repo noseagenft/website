4b8b93fa6396abe236ba45abf7ee6a25febdb604
nosenudge <noseagenft@gmail.com>
2022-03-17 10:21:46 +0800

    WIP

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,35 @@
-import { Button } from '../Button/Button';
-import './About.css';
+import React, { useEffect } from "react";
+import { Button } from "../Button/Button";
+import "./About.css";
 
-export const About = () =>
-  <section className="About">
-    <Button>asdas</Button>
-  </section>
+export const About = () => {
+  useEffect(() => {
+    const el = document.querySelector("#About");
+
+    el.addEventListener("mousemove", (e) => {
+      el.style.backgroundPositionX = -e.offsetX * 0.5 + "px";
+      el.style.backgroundPositionY = -e.offsetY * 0.5 + "px";
+    });
+  }, []);
+
+  return (
+    <section className="About" id="About">
+      <div className="About-top">
+        <div className="About-top-description">
+          <div className="About-top-description-title">
+            <span>The</span> <span>3,456</span> <span>Noses</span>
+          </div>
+          <div className="About-top-description-body">
+            Bearing only marks in homage to their lineage, decided against their
+            originators' war-mongering nature and started living in harmony.
+            Each brings to the table the best they can offer, in exchange only
+            for camaraderie. Instead of competing for resources, the Noses got
+            together to figure out how to leverage each others' strengths to
+            advance as a society.
+          </div>
+        </div>
+        <Button>asdas</Button>
+      </div>
+    </section>
+  );
+};
